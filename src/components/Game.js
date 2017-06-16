2d77738c720da6e8c10a55eb18088b8fdb8a4820
Monica Williams <monicaestellaw@gmail.com>
2017-06-16 10:26:36 -0700

    rescaffolded file system, implemented react-router-dom because hashHistory works with github pages but react-router's browserHistory does not.

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game">
+        <h1 className="hi"> This is the start of the game </h1>
+      </div>
+    )
+  }
+}
+
+export default Game
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Game from './components/Game'
+import About from './components/About'
+import CurrentProject from './components/CurrentProject'
+import PastProjects from './components/PastProjects'
+import Contact from './components/Contact'
+import { HashRouter, Route, Link } from 'react-router-dom'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render((
+  <HashRouter>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/game" component={Game}/>
+      <Route path="/about" component={About}/>
+      <Route path="/currentproject" component={CurrentProject}/>
+      <Route path="/pastProjects" component={PastProjects}/>
+      <Route path="/contact" component={Contact}/>
+    </div>
+  </HashRouter>
+),
+document.getElementById( 'root' ))
+registerServiceWorker()

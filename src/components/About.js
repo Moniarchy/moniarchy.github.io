b614b60b044b7de749721ebe0cb278c2191bb86c
Monica Williams <monicaestellaw@gmail.com>
2017-08-29 23:27:06 -0700

    adds headshot image, about description, and links to external media

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import './About.css'
+import headshot from './LinkedInHeadshot.jpg'
+
 
 class About extends Component {
   render() {
     return (
       <div className="About">
-        <h1 className="hi"> Learn about me </h1>
+        <img src={headshot} className="headshot"></img>
+        <h1 className="hi"> My name is Monica. I am a full-stack developer from Oakland. I speak Javascript (NodeJS, ExpressJS, ReactJS), I test my code (MochaJS, ChaiJS), and I know how to use a database (PostgreSQL, MongoDB).</h1>
+        <a href="https://www.linkedin.com/in/monica-williams-25582a36/" className="linkedin">Connect with me on LinkedIn</a>
+        <a href="https://github.com/Moniarchy" className="github">See my code</a>
+        <a href="https://medium.com/@monicaestellawilliams" className="medium">Follow me on Medium</a>
       </div>
     )
   }
